7b044009cc45fe0ca63ff7d5a49777280ebb945d
max683879@hotmail.com <max683879@hotmail.com>
2024-05-19 08:40:57 +0800

    1.all module unit-test

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from datetime import timedelta
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-hard-to-guess-string'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -17,3 +16,22 @@ class Config:
 
     # Pagination
     POSTS_PER_PAGE =10
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory'  #
+    TESTING = True
+
+    WTF_CSRF_ENABLED = False  # ban CSRF protection
+
+    SERVER_NAME = '127.0.0.1:5001' # Using an in-memory database
+
+    APPLICATION_ROOT = '/'
+
+    PREFERRED_URL_SCHEME = 'http'
+
+class SeleniumTestingConfig(TestConfig):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'tests', 'testapp.db')
+
